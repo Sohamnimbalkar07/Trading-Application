cb7022d8a2e49e76dc62c838277423fef5c43814
Soham Nimbalkar <sohamnimbalkar07@gmail.com>
2025-01-15 09:48:54 +0530

    implement rediscallbackhandler and emit functions to send updates to the subscribed users

--- ws/src/User.ts
+++ ws/src/User.ts
@@ -1,4 +1,7 @@
 import { WebSocket } from "ws";
+import { IncomingMessage, SUBSCRIBE, UNSUBSCRIBE } from "./types/in";
+import { SubscriptionManager } from "./SubscriptionManager";
+import { OutgoingMessage } from "./types/out";
 
 export class User {
   private id: string;
@@ -7,5 +10,37 @@ export class User {
   constructor(id: string, ws: WebSocket) {
     this.id = id;
     this.ws = ws;
+    this.addListeners();
+  }
+
+  private subscriptions: string[] = [];
+
+  public subscribe(subscription: string) {
+    this.subscriptions.push(subscription);
+  }
+
+  public unsubscribe(subscription: string) {
+    this.subscriptions = this.subscriptions.filter((s) => s !== subscription);
+  }
+
+  emit(message: OutgoingMessage) {
+    this.ws.send(JSON.stringify(message));
+  }
+
+  private addListeners() {
+    this.ws.on("message", (message: string) => {
+      const parsedMessage: IncomingMessage = JSON.parse(message);
+      if (parsedMessage.method === SUBSCRIBE) {
+        parsedMessage.params.forEach((s) =>
+          SubscriptionManager.getInstance().subscribe(this.id, s)
+        );
+      }
+
+      if (parsedMessage.method === UNSUBSCRIBE) {
+        parsedMessage.params.forEach((s) =>
+          SubscriptionManager.getInstance().unsubscribe(this.id, s)
+        );
+      }
+    });
   }
 }
