79ca0685bb37e7e1d2c473d980cf52f5c6c144ef
Soham Nimbalkar <sohamnimbalkar07@gmail.com>
2025-01-20 22:40:17 +0530

    complete market bar UI

--- frontend/components/layout/Marketbar.tsx
+++ frontend/components/layout/Marketbar.tsx
@@ -2,8 +2,29 @@
 
 export const Marketbar = ({ market }: { market: string }) => {
   return (
-    <div className="h-14 px-4 bg-black flex items-center">
+    <div className="h-16 px-4 bg-black flex items-center gap-10">
       <Ticker market={market} />
+      <div className="font-black text-white">TATA</div>
+      <div className="flex flex-col text-white items-center">
+      <div className="font-normal">0.2325</div>
+      <div className="font-semibold">$ 0.2325</div>
+      </div>
+      <div className="flex flex-col  items-center">
+      <div className="font-normal text-zinc-400">24H Change</div>
+      <div className="text-white font-semibold">+ 0.2325</div>
+      </div>
+      <div className="flex flex-col items-center">
+      <div className=" text-zinc-400 font-normal">24H High</div>
+      <div className="text-white font-semibold">$ 0.2325</div>
+      </div>
+      <div className="flex flex-col items-center">
+      <div className="text-zinc-400 font-normal">24H Low</div>
+      <div className="text-white font-semibold">$ 0.2325</div>
+      </div>
+      <div className="flex flex-col items-center">
+      <div className="text-zinc-400 font-normal">24H Volume</div>
+      <div className="text-white font-semibold">$ 0.2325</div>
+      </div>
     </div>
   );
 };
